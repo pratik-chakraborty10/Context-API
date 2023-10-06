099180307dd5dc0276e69a8ac6150e0ce8d0fa1c
Pratik Chakraborty <118681019+pratik-chakraborty10@users.noreply.github.com>
2023-10-07 00:53:24 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './Context/UserContextProvider'
+
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
